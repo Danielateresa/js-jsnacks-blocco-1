b6f51696ab796f22baffb84d75b25f3804ee94f8
Danielateresa <dany.t_87@hotmail.it>
2022-10-12 12:43:40 +0200

    Not completed

---
+++ jsnack3/script.js
@@ -0,0 +1,13 @@
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+let userListNumber
+for (i = 0; i < 10; i++) {
+    const userNumbers = Number(prompt('inserisci un numero'));
+    console.log(userNumbers);
+    userListNumber = userNumber[i]
+
+    console.log(userListNumber);
+
+
+}
